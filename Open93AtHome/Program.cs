cea18de35ec6e854ecdfbd196d1761746d32dbf1
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-06-15 23:42:41 +0800

    add: 添加 todo 以理清思路，安装基础的包

--- Open93AtHome/Program.cs
+++ Open93AtHome/Program.cs
@@ -1,10 +1,14 @@
 ﻿namespace Open93AtHome
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Starting {nameof(Open93AtHome)} center server");
+            //TODO: 读取节点信息
+            //TODO: 开启 Http 服务和 SocketIO
+            //TODO: 启动监听线程
+            //TODO: 阻塞主线程使得程序不会立刻退出
         }
     }
 }
